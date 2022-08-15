eecd75e6bc09374e7605333b5a30a85ed755cd82
Rakku <dudfkr7845@gmail.com>
2022-08-16 01:10:07 +0900

    Update #3 - Main
    변수 클래스를 인터페이스로 변경

--- java-oop/src/main/java/com/rakku/sort/Main.java
+++ java-oop/src/main/java/com/rakku/sort/Main.java
@@ -1,14 +1,16 @@
 package com.rakku.sort;
 
 import com.rakku.sort.logic.BubbleSort;
+import com.rakku.sort.logic.JavaSort;
+import com.rakku.sort.logic.Sort;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> bubbleSort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>();
 
-        System.out.println("정렬 결과 : " + bubbleSort.sort(Arrays.asList(args)));
+        System.out.println("정렬 결과 : " + sort.sort(Arrays.asList(args)));
     }
 }
